5c23348c883baed0fbca066477b28aa66c80b751
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-12-03 09:48:20 -0800

    the portfolio

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,26 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="m-0 h-full grid place-items-center bg-black no-underline">
       <Head>
-        <title>Create Next App</title>
+        <title>
+          {process.env.NEXT_PUBLIC_DOMAIN
+            ? NEXT_PUBLIC_DOMAIN.env.DOMAIN
+            : process.env.NEXT_PUBLIC_USERNAME}
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="grid place-items-center text-center">
+        <div>
+          <h1 className="font-sans text-7xl text-white tracking-tighter">
+            Hi, I'm {process.env.NEXT_PUBLIC_USERNAME}
+          </h1>
+          <p className="font-comfortaa text-indigo-600 text-2xl my-1.5">
+            Blogger <span className="text-white">|</span> Developer
+          </p>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./pages/**/*.js"],
+  darkMode: false,
+  theme: {
+    fontSize: {
+      "7xl": "5rem",
+    },
+    fontFamily: {
+      sans: ['"Source Sans Pro"', "sans-serif"],
+      comfortaa: ["Comfortaa", "cursive"],
+    },
+  },
+  variants: {},
+  plugins: [],
+};
